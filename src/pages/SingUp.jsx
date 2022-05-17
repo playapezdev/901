14b7887749f13393b72201883c2721b1193b62df
Gabriel <gsosa4812@gmail.com>
2022-05-17 10:18:52 -0500

    Componente para crear usuario

--- src/pages/SingUp.jsx
+++ src/pages/SingUp.jsx
@@ -1,11 +1,25 @@
 import React, { Fragment } from "react";
+import "../styles/SingUp.scss";
 
 export default function CrearCuenta() {
   return (
     <Fragment>
-      <div>
-
-          
+      <div className="form-General">
+        <div className="form-principio">
+          <label>Crear Cuenta</label>
+        </div>
+        <form className="form-bloques">
+          <div>
+            <label>Usuario</label>
+            <div className="grupo-input">
+              <input
+                className="for-input"
+                placeholder="usuario"
+                type="text"
+              ></input>
+            </div>
+          </div>
+        </form>
       </div>
     </Fragment>
   );
